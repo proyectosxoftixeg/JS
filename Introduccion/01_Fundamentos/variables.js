c68f264bc796388a1c33270670814aaa1428a3c6
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-01-23 12:02:02 -0500

    Fundamentos_1

--- Introduccion/01_Fundamentos/variables.js
+++ Introduccion/01_Fundamentos/variables.js
@@ -1,3 +1,15 @@
 // console.log('Comentarios')
 
 // Variables es un contenedor de informacion que apunta a un lugar en la memoria, dicha inforamcion puede cambiar en el futuro
+
+let a =10; 
+var b = 20;
+
+
+// c = 20; el valor de una variable constante no se puede modificar porque se genera error 
+
+
+
+let c =10, d=20, e=30, f=15, g = c + d;
+document.write(g);
+
