52b0093babf1981bf40a6a2eadb3660cad573daa
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-02-10 12:26:43 -0500

    palabras-reservadas

---
+++ Introduccion/Primitivos/palabras_reservadas.js
@@ -0,0 +1,38 @@
+let numero = 123;
+
+/** Las palabras reservadas para JS SON:
+ * await
+ * break
+ * case
+ * catch
+ * class
+ * const
+ * continue
+ * debugger
+ * default
+ * delete
+ * do
+ * else
+ * export
+ * extends
+ * finally
+ * for
+ * function
+ * if
+ * import
+ * in
+ * instanceof
+ * new
+ * return
+ * super
+ * switch
+ * this
+ * throw
+ * try
+ * typeof
+ * var
+ * void
+ * while
+ * with
+ * yield
+ * **/
