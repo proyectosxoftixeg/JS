0d8f07eca5a7450ec6ee5208cd868cf057dc0686
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-02-11 05:36:09 -0500

    acceder_array

---
+++ Introduccion/Primitivos/practica_arreglos.js
@@ -0,0 +1,25 @@
+let marcas = ['Sony','Apple','Microsoft','Intel','Google','Amazon','Nvidia'];
+console.log(marcas);
+
+
+// Operaciones Habituales con un Array Metodos
+//Acceder a un elemento del array
+
+let primero = (marcas[0]);
+console.log(primero,marcas);
+let segundo = (marcas[1]);
+console.log (segundo,marcas);
+let tercero = (marcas[2]);
+console.log (tercero,marcas);
+let cuarto = (marcas[3]);
+console.log(cuarto,marcas);
+let quinto = [marcas[4]];
+console.log(quinto,marcas);
+let sexto = (marcas[5]);
+console.log(sexto,marcas);
+let septimo = (marcas[6]);
+console.log(septimo,marcas);
+
+
+
+
