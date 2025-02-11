12bb3323dee1599e53372d1752eca4fa57e7ec40
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-02-10 19:33:14 -0500

    arreglos_1

---
+++ Introduccion/Primitivos/arreglos.js
@@ -0,0 +1,21 @@
+// const arr = new Array(10);
+// console.log(arr)
+
+let videoJuegos = ['Mario3', 'Megaman', 'Chromo', 'Trigger'];
+console.log(videoJuegos);
+
+console.log(videoJuegos[0]);
+let arregloCosas = [
+    true,
+    123,
+    'fernando',
+    1 + 2,
+    function(){},
+    ()=>{},
+    {a:1},
+    ['X', 'Megaman', 'Zero', 'Dr. Ligth',[1,'juan','Pedro', false]],
+];
+console.log(arregloCosas[7][4][3])
+
+
+
