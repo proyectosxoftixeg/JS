1e594005a6407b78694ac2c313c333d616812d22
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-02-14 07:38:19 -0500

    Curso_Html

--- Introduccion/Primitivos/practica_arreglos.js
+++ Introduccion/Primitivos/practica_arreglos.js
@@ -20,6 +20,30 @@ console.log(sexto,marcas);
 let septimo = (marcas[6]);
 console.log(septimo,marcas);
 
+// saber la longitud de un array
+
+let longitudMarcas = (marcas.length);
+console.log("La longitud del arreglo marcas es: ",longitudMarcas);
+
+// Conocer ultima posicion
+
+let ultimoMarca = marcas [marcas.length -1];
+console.log ('La ultima marca del array es: ',ultimoMarca, marcas);
+
+
+//Recorrer un array
+let recorrer = marcas.forEach((elemento,indice,arr)=>{
+    console.log(elemento,indice);
+})
+
+let recorre = marcas.forEach((elemento,indice,arr)=>{
+    console.log(elemento,indice);
+})
+
+let recorr = marcas.forEach((elemento,indice,arrerglo)=>{
+    console.log(elemento,indice,arrerglo);
+})
+
 
 
 
