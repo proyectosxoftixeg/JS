a6ca314dbc7773d0262ae7675b8ab1c6d5386b31
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-02-27 20:56:27 -0500

    Ejemplo

---
+++ JS_Platzi/reto.js
@@ -0,0 +1,7 @@
+let array = [1,2,3];
+let maxima = 0;
+for (i = 0; i < array.length; i++);
+maxima = Math.max(maxima , array[i]);
+
+
+
---
+++ JS_Platzi/script.js
@@ -0,0 +1,24 @@
+// Calculadora realizando operaciones SUMA, RESTA, MULTIPLICACION Y DIVICION
+
+function suma(){
+    resultado = 6+5;
+    console.log(suma);
+}
+suma()
+
+function resta(){
+    resultado = 6 - 5;
+    console.log(resultado)
+}
+resta()
+function multiplicacion(){
+    resultado = 5 * 6 ;
+    console.log(resultado)
+}
+multiplicacion()
+function division (){
+    resultado = 25 / 5;
+    console.log(resultado)
+}
+division()
+
