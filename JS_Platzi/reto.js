cf21de09a4ce2b5113d94b82f4b1131809f13435
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-03-03 20:08:00 -0500

    Practicas

--- JS_Platzi/reto.js
+++ JS_Platzi/reto.js
@@ -1,7 +1,23 @@
-let array = [1,2,3];
-let maxima = 0;
-for (i = 0; i < array.length; i++);
-maxima = Math.max(maxima , array[i]);
+let numeros = [2,4,5,1,100,6,10];
+let max = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    if(max < numeros[i] ){
+        max = numeros[i];
+    }
+}
+
+console.log("El valor mas grande de max es: " + max);
 
 
+
+let arr = [1,2,3];
+let maximo = 0;
+
+for(let i=0; i < arr.length; i++){
+    if(maximo < arr[i]){
+        maximo = arr[i]
+    }
+}
+console.log(maximo);
 
---
+++ Python/script.py
@@ -0,0 +1,8 @@
+## ESTE ES UN COMENTARIO EN PYTHON
+
+valor1 = 7
+valor2 = 2
+resultadoSuma = valor1 + valor2
+print("Suma", resultadoSuma)
+resultadoResta = valor1 - valor2
+print("Resta", resultadoResta)
